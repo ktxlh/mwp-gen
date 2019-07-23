9c87a03cfdd89afc05e30ec18e61144949cdb85a
Kate Hsu <shanglinghsu@gmail.com>
2019-07-23 17:39:09 +0800

    Update deleted

--- download.sh
+++
@@ -1,2 +0,0 @@
-scp shangling@140.109.19.51:~/ntg2/command_tips.sh .
-bash command_tips.sh
--- upload.sh
+++
@@ -1,4 +0,0 @@
-echo "Upload files to ~/ntg2/"
-scp ~/mwp/ntg2/*.{py,sh} shangling@140.109.19.51:~/ntg2/
-echo "Upload files to ~/Datasets/"
-scp ~/mwp/Datasets/*.py shangling@140.109.19.51:~/Datasets/
